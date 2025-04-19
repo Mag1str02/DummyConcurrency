35949857aec3e7b8a81ddb67313da256c9108557
Mag1str0 <rysin_d@mail.ru>
2025-04-19 15:38:00 +0300

    Fix exeption context

--- DummyConcurrency/Context/Exceptions/nop/Context.hpp
+++ DummyConcurrency/Context/Exceptions/nop/Context.hpp
@@ -8,7 +8,7 @@ namespace DummyConcurrency::Context {
 
     struct ExceptionsContext {
         void SwitchTo(ExceptionsContext& /*target*/) {
-            DC_ASSERT(!std::uncaught_exception(),
+            DC_ASSERT(std::uncaught_exceptions() == 0,
                       "Program was build with nop exception context, but is required to switch context while there is an exception in flight");
         }
     };
